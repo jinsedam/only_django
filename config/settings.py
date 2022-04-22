2c413c8cb03b50b4019cf2889db70b6cf85da539
jinsedam <jinsedam@naver.com>
2022-04-22 11:23:45 +0900

    init

--- config/settings.py
+++ config/settings.py
@@ -133,6 +133,10 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [ BASE_DIR / 'static']
 
+MEDIA_URL = '/uploads/'
+
+MEDIA_ROOT = [BASE_DIR / 'uploads']
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
